f33f5ffcfaa2fed02933a0da9dd3a09268278593
Tien <tutrongtien.tt@gmail.com>
2017-10-12 18:50:26 +0700

    session timeout

---
+++ database/migrations/2017_10_12_052738_add_rememner_token_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRememnerTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('remember_token')->after('country')->nullable();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('remember_token');
+        });
+    }
+}
